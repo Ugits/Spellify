fa365d302519200cab5d317807c68f9f7099a624
Jonas Ullvahn <ullvahn@gmail.com>
2024-09-17 19:36:50 +0200

    Initial commit

---
+++ src/main/java/org/jonas/spellify/SpellifyApplication.java
@@ -0,0 +1,13 @@
+package org.jonas.spellify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpellifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jonas/spellify/SpellifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jonas.spellify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
