7ef254371a30e96f374e1f73c5831ca4dfb958ad
Jonas Ullvahn <ullvahn@gmail.com>
2024-09-19 10:10:01 +0200

    ADD entity model

---
+++ src/main/java/org/jonas/spellify/model/entity/Spell.java
@@ -0,0 +1,118 @@
+package org.jonas.spellify.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(schema = "spells")
+public class Spell {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String index;
+    private String name;
+    @JsonProperty("casting_time")
+    private String castingTime;
+    @JsonProperty("desc")
+    private List<String> description;
+    private Integer level;
+    private String range;
+    private Boolean ritual;
+    private String duration;
+    private Boolean concentration;
+
+    public Spell() {
+    }
+
+    public Spell(String name, List<String> description, int level, String range, boolean ritual, String duration, boolean concentration, String castingTime) {
+        this.name = name;
+        this.description = description;
+        this.level = level;
+        this.range = range;
+        this.ritual = ritual;
+        this.duration = duration;
+        this.concentration = concentration;
+        this.castingTime = castingTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getDescription() {
+        return description;
+    }
+
+    public void setDescription(List<String> description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public boolean isRitual() {
+        return ritual;
+    }
+
+    public void setRitual(boolean ritual) {
+        this.ritual = ritual;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public boolean isConcentration() {
+        return concentration;
+    }
+
+    public void setConcentration(boolean concentration) {
+        this.concentration = concentration;
+    }
+
+    public String getCastingTime() {
+        return castingTime;
+    }
+
+    public void setCastingTime(String castingTime) {
+        this.castingTime = castingTime;
+    }
+
+
+}
