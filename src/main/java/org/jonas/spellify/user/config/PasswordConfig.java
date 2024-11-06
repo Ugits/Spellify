032a129ab102bd97323bbf0cdc285a40ed7f8180
Jonas Ullvahn <ullvahn@gmail.com>
2024-11-06 18:11:36 +0100

    Add Password Encoder Bean

---
+++ src/main/java/org/jonas/spellify/user/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package org.jonas.spellify.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(15);
+    }
+}
--- src/main/java/org/jonas/spellify/user/model/entity/CustomUser.java
+++ src/main/java/org/jonas/spellify/user/model/entity/CustomUser.java
@@ -1,5 +1,4 @@
 package org.jonas.spellify.user.model.entity;
-
 import jakarta.persistence.*;
 
 @Entity
@@ -8,11 +7,37 @@ public class CustomUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String username;
 
     private String password;
 
+    public CustomUser() {
+    }
 
+    public CustomUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
